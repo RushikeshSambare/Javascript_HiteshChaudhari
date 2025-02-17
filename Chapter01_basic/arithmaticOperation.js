1498720722a59fd7da1178e5b25f05d2a0764548
Rushikesh  B Sambare <107785063+RushikeshSambare@users.noreply.github.com>
2025-02-17 00:49:29 +0000

    concept of prefix and postfix

---
+++ Chapter01_basic/arithmaticOperation.js
@@ -0,0 +1,16 @@
+let value = 3;
+let NegValue = - value;
+console.log(NegValue);   // -3
+
+console.log(2**2); //4
+console.log(2**3); //8
+
+//postfix
+let a = 10;
+console.log(a++); //1o
+console.log(a++); //11
+
+//prefix
+let b =10;
+console.log(++b); //11
+console.log(++b);  //12
